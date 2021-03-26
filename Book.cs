cdbacbb17f3fd36d4e3f20a1e1868cb8efa72c73
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-25 19:49:07 -0500

    first commit

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//  Classes & Objects | C# | Tutorial 25
+// data types - text with string bool whole numbers int double floats 
+// are limited and we need more to
+// create your own custom data types with classes and objects 
+// class is just a specification for a new data type - blue print for the data
+// give the class some attributes to define what that class is
+// for each attribute we have we create a variable 
+namespace CSharpExercises_25
+{
+    class Book
+    {
+        // attributes describe the class
+        public string title;
+        public string author;
+        public int pages;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpExercises_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object - instance of the class
+
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord Of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+
+
+            Console.WriteLine("Title is " + book1.title);
+            Console.WriteLine("Author is " + book1.author);
+            Console.WriteLine("Pages in book is "  + book1.pages);
+
+            Console.WriteLine("Title is " + book2.title);
+            Console.WriteLine("Author is " + book2.author);
+            Console.WriteLine("Pages in book is " + book2.pages);
+
+            Console.ReadLine();
+        }
+    }
+}
